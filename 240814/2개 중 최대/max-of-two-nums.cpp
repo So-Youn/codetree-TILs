5a8d992258a3ffd7f0c7dcf889df40f12c127450
SY <58545284+So-Youn@users.noreply.github.com>
2024-08-14 11:14:01 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240814/2개 중 최대/max-of-two-nums.cpp
+++ 240814/2개 중 최대/max-of-two-nums.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b, n;
+    int a, b, max;
     cin >> a >> b;
 
-    n = (a > b)? a : b;
-    cout << n;
+    max = (a > b)? a : b;
+    cout << max;
     return 0;
 }
