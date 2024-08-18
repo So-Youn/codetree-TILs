391d46a302591db35152a25e11ffd30a95d33b76
SY <58545284+So-Youn@users.noreply.github.com>
2024-08-18 18:38:40 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240818/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    char n;
+    cin >> a;
+   
+    
+    n= (a=1)? 't' : 'f';
+    cout << n;
+    return 0;
+}
