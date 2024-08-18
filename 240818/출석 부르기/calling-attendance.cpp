3cd05e0cfd6956f2bbd93b89acbfa0bc5938d045
SY <58545284+So-Youn@users.noreply.github.com>
2024-08-18 18:58:34 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240818/출석 부르기/calling-attendance.cpp
+++ 240818/출석 부르기/calling-attendance.cpp
@@ -6,11 +6,11 @@ int main() {
     int a;
     cin >> a;
     
-    if (a=1)
+    if (a==1)
         cout << "John";
-    else if (a=2)
+    else if (a==2)
         cout << "Tom";
-    else if (a=3)
+    else if (a==3)
         cout << "Paul";
     else
         cout << "Vacancy";
