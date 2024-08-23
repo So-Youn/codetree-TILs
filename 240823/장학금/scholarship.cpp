35dc546cf0aaac834ab2e11c730f3ab696b48f29
SY <58545284+So-Youn@users.noreply.github.com>
2024-08-23 23:51:06 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240823/장학금/scholarship.cpp
+++ 240823/장학금/scholarship.cpp
@@ -6,14 +6,11 @@ int main() {
     int mid_score, fin_score;
     cin >> mid_score >> fin_score;
 
-    if (mid_score >= 90)
-        if (fin_score >= 95 && fin_score <= 100)
-            cout << "100000";
-        else if ( fin_score >= 90)
-            cout << "50000";
-        else
-            cout << 0;
+    if (mid_score >=90 && fin_score >= 95)
+        cout << "100000";
+    else if (mid_score >=90 && fin_score >= 90)
+        cout << "50000";
     else
-        cout << 0;
+        cout << "0";
     return 0;
 }
